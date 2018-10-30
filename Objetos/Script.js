ba369c42a292e2361cff30f6528b5184322bb6ba
Peio Murguia <peiomurguia@gmail.com>
2018-10-30 16:29:34 +0100

    Commit

---
+++ Objetos/Script.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+let perro = {
+    nombre:"Kyra"
+};
+
+let gato = {
+    nombre: "Lucifer"
+};
+
+let animales = [perro, gato];
+
+for (let i = 0; i < animales.length; i++) {
+    console.log(animales[i].nombre);
+}
